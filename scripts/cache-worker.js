878b24af56c2d342ec9480a87fe57b6639ff7c23
Nekit <nick.misiura@yandex.ru>
2026-06-27 11:38:35 +0300

    Do not cache PWA icon and manifest

--- scripts/cache-worker.js
+++ scripts/cache-worker.js
@@ -4,8 +4,6 @@ const precachedResources = [
     "/json-to-srt-converter-pwa/index.html",
     "/json-to-srt-converter-pwa/index.js",
     "/json-to-srt-converter-pwa/styles.css",
-    "/json-to-srt-converter-pwa/manifest.json",
-    "/json-to-srt-converter-pwa/icons/pwa-icon-512.png",
     "/json-to-srt-converter-pwa/icons/favicon.png",
     "/json-to-srt-converter-pwa/scripts/converter.js",
     "/json-to-srt-converter-pwa/scripts/dom-handler.js",
